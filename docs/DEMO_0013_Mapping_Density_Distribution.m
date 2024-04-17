4cc04cabdbba6fea0c0b71e3b18ac5d05ec31dbe
mahtab-vafaee <mahtab.vafaee@gmail.com>
2024-04-17 18:14:54 +0100

    Adding DEMO_13/14_Denisity_Mappings

--- docs/DEMO_0013_Mapping_Density_Distribution.m
+++ docs/DEMO_0013_Mapping_Density_Distribution.m
@@ -1,9 +1,9 @@
 %% DEMO_0013_Mapping_Density_Distribution
 % This is a demo for:
 % 
-% * Building a non-uniform infill lattice structure, to map a specific 
-% structural properties, e.g. in this demo, it is mapping a density 
-% distribution field within the domain.
+% * Building a graded lattice structure, to map a specific 
+% structural properties on a rectangular domain, 
+% e.g. in this demo, it is mapping a density distribution field.
 %
 %%  
 % _*Name*_ 
@@ -15,10 +15,10 @@
 %  Change log:
 %  2023/11/15 MV Created  
 %  2024/02/06 MV Edited
+%  2024/04/15 MV/KM Revised
 % ----------------------------------------------------------------------
 %%
-
-clc; clear all; close all;
+clc; clear; close all;
 
 %% Plot settings
 
@@ -30,165 +30,98 @@ edgeColor2='none';
 fontSize=25; 
 pColors=gjet(6);
 
-%% creating density field
-
+%% Using topology optimisation to create a density field
 % define box size, volume fraction as inputs for top.m
-nelx = 50;
-nely = 25;
-nelz = 25;
-volfrac = 0.7;
-penal = 2;
-rmin = 6;
+s=1; % s=3
+nelx = s*64;
+nely = s*24;
+nelz = s*24;
 
-% calculating the density distribution map
+volfrac = 0.5;
+penal = 3;
+rmin = 10;
+
+%% TOP: calculating the density distribution map
 [rho0] = top(nelx,nely,volfrac,penal,rmin);
 
-% extending through the z-direction
+%% extending through the z-direction
 ext = [1, 1, nelz];
 rho = repmat(rho0, ext); 
-sv3(rho); colormap warmcold; %Visualize 3D field
 
-%% grid for Phi-field
+%% Visualize 3D model and density data
+sv3(rho); colormap warmcold; 
 
-n = 5; % resolution scale factor
-x = linspace (0,nelx,nelx*n); 
-y = linspace (0,nely,nely*n); 
-z = linspace (0,nelz,nelz*n); 
+%% Defining desired spacing (e.g. as per desired spatial frequency) for grids
 
-[XG, YG, ZG] = meshgrid(x, y, z); %Creating grid
+spatialWaveLength = 5; % Nominal spatial wave length
+samplingRate = 3; % Rate per "wave"
+v = spatialWaveLength/samplingRate;
 
-VG= [XG(:), YG(:), ZG(:)];
+%% Creating a grid with desired voxel size for Gyroid image
 
-%% creating hex mesh 
-boxDim=[nelx nely nelz];
-boxEl=[nelx nely nelz];
+w=linspace(0,nelx,nelx);
+d=linspace(0,nely,nely);
+h=linspace(0,nelz,nelz);
 
-[meshStruct]=hexMeshBox(boxDim,boxEl); %creating elements
+[X,Y,Z]=meshgrid(w,d,h); 
 
-%mesh structure
-E=meshStruct.E;
-V=meshStruct.V;
-F=meshStruct.F;
-Fb=meshStruct.Fb;
-faceBoundaryMarker=meshStruct.faceBoundaryMarker;
+% grids with desired resolution v
+[XG, YG, ZG] = meshgrid(linspace(0,nelx,v*nelx),...
+    linspace(0,nely,v*nely),...
+    linspace(0,nely,v*nely));
 
-V=V-min(V,[],1); %shifting to origin
+%% Interpolating data on the grid
 
-%% Visualization
+rho_VG = griddata(X,Y,Z,rho,XG(:),YG(:),ZG(:),'linear'); %Interpolate on grid
+rho_VG = reshape(rho_VG,size(XG));
 
-cFigure; hold on;
-title('Box boundaries faces','FontSize',fontSize);
-
-gpatch(Fb,V,'kw','k',faceAlpha1, 0.5);
-scatter3(V(:,1),V(:,2),V(:,3),20,'red');
-% scatter3(VG(:,1),VG(:,2),VG(:,3),3,'y');
-
-axisGeom(gca,fontSize);
-colormap(gjet(6)); icolorbar;
-drawnow;
-
-%% Access model element and patch data
-
-% Fb=boundary(Vd(:,1), Vd(:,2), Vd(:,3),1); % boundary with shrink-factor 1(not the same as faceBoundary)
-indBoundary=tesBoundary(Fb);
-Fb=Fb(indBoundary,:);
-Cb=-1*ones(size(Fb,1),1);
-CE=-2*ones(size(E,1),1);
-
-meshStructure.elementData=rho; % Copy density as the element data
-
-% Access model element and patch data
-meshStructure.facesBoundary=Fb;
-meshStructure.boundaryMarker=Cb;
-meshStructure.nodes=V;
-meshStructure.elementMaterialID=CE;
-meshStructure.elements=E;
-
-%% Mesh output
-
-meshView(meshStructure);
-
-%% Use barycentric mapping to figure out the elements voxel centres are found in
-% In this exampe, the 8-node elements are generated to hold the grids inside
-
-% TR = triangulation(E,V); %Conver to "triangulation" type
-% [elementIndexFound,baryCentricCoordinate]=pointLocationTR(TR,VG,1,1,1); % Compute 
-% logicInside=~isnan(elementIndexFound); %if nan then the voxels are outside shape 
-
-%% Interpolation of rho onto grid
-
-interpolationMethod = 2;
-
-switch interpolationMethod
-    case 1 % Neirest
-        rho_VG=nan(size(VG,1),1);
-        rho_VG(logicInside)=rho(elementIndexFound(logicInside));
-        rho_VG=reshape(rho_VG, size(XG));
-
-    case 2 % Linear
-        Vm=patchCentre(E,V); % center of the elements
-        rho_VG=griddata(Vm(:,1),Vm(:,2),Vm(:,3),rho,XG(:),YG(:),ZG(:),'linear'); %Interpolate on grid
-
-    case 3 % Element nodal average tri-linear
-        rho_V = faceToVertexMeasure(E,V,rho); % Average from elements to nodes
-        % Shape function (=barycentric coordinate) based within element
-        % tri-linear interpolation
-        rho_VG=nan(size(VG,1),1);
-        for indPoint = find(logicInside)'
-            indElement =  elementIndexFound(indPoint);
-            indNodes = E(indElement,:);
-            rho_VG(indPoint) = sum(baryCentricCoordinate(indPoint,:) .* rho_V(indNodes)');
-        end
-
-    case 4 % Natural
-        Vm=patchCentre(E,Vi); % center of the elements
-        interpFunc_rho = scatteredInterpolant(Vm,rho,'natural','none'); %Create interpolator
-        rho_VG=nan(size(VG,1),1);
-        rho_VG(logicInside) = interpFunc_rho(VG(logicInside,:));        
-end
-
-rho_VG=reshape(rho_VG, size(XG)); %reshape to the density size
-sv3(rho_VG); colormap gjet; 
+%% Visualize interpolated density on the grid
+sv3(rho_VG); colormap warmcold;
+caxis([0,1]);
 
 %% Mapping the density field to the equivalent gyroid levelSet field
 
-l=(rho_VG-0.5)/-(1/3); % [-1.5, 0.75] levelset range 
-freq=0.05; %gyroid cell size
-k = freq*boxDim; % number of periods
+l=(rho_VG-0.5)/-(1/3); % Equivalent levelset image for rho
+k = (1*pi)/spatialWaveLength; % number of periods
 
+%% Visualizig levelSet data
 cFigure;  
-scatter3(VG(:,1),VG(:,2),VG(:,3),25,l(:),'filled');
+scatter3(XG(:),YG(:),ZG(:),20,l(:),'filled');
 axis tight; axis equal; 
 colorbar;
 
 %% Evaluate triply periodic function
 
-S=(sin(k(1,1).*XG).*cos(k(1,2).*YG))+(sin(k(1,2).*YG).*cos(k(1,3).*ZG))+(cos(k(1,1).*XG).*sin(k(1,3).*ZG)); 
+S=(sin(k.*XG).*cos(k.*YG))+(sin(k.*YG).*cos(k.*ZG))+(cos(k.*XG).*sin(k.*ZG)); 
 
-i=2.5; % Leads to a minimu of 1 later
-S=S+i; % S=-S-i;
+sv3(S); colormap warmcold;
+
+%% Mapping the density field to the equivalent gyroid levelSet field
+% Gyroid "spans" -1.5-1.5, shift so minimum is at 1
+i=2.5; % Leads to a minimum of 1 later
+Sg=S+i; % S=-S-i;
 l=l+i; % l=-l-i; % i:i+s
-S=S./l; 
+Sg=Sg./l; 
+
+Sg=Sg-1; % bringing back to zero levelset
 
 %visualize gyroid function field
 sv3(S); colormap warmcold;
 
 %% Construct iso-surface
 
-[Fg,Vg] = isosurface(XG,YG,ZG,S,1);
-[Ft,Vt]=quad2tri(Fb,V,'f'); %Quad patch to triangular patch data
+[Fg,Vg] = isosurface(XG,YG,ZG,S,0);
+[Fgc,Vgc] = isocaps(XG,YG,ZG,S,0);
 
-[Fsn,Vsn,Csn]=joinElementSets({Ft,Fg},{Vt,Vg});
+%% Joining the two surfaces
+
+[Fsn,Vsn,Csn]=joinElementSets({Fg,Fgc},{Vg,Vgc});
 [Fsn,Vsn]=patchCleanUnused(Fsn,Vsn); %Remove unused nodes
 [Fsn,Vsn]=mergeVertices(Fsn,Vsn); %Merge nodes
 
-% Visualize surface
+%% Visualize surface
 cFigure; 
-gpatch(Fsn,Vsn,'kw','none',0.3);
+gpatch(Fsn,Vsn,'kw','none',1);
 axisGeom; colormap spectral; icolorbar; 
-camlight headlight; axis off;
+camlight headlight; axis on;
 drawnow; 
-
-%% Slicer view
-
-cutViewAnim(Fsn,Vsn);
---
+++ docs/DEMO_0014_Mapping_Density_Distribution_with_Shell.m
@@ -0,0 +1,226 @@
+%% DEMO_0014_Mapping_Density_Distribution_with_Shell
+% This is a demo for:
+% 
+% * Building a graded lattice structure, to map a specific 
+% structural properties on a rectangular domain, 
+% e.g. in this demo, it is mapping a density distribution field.
+% 
+%%  
+% _*Name*_ 
+% 
+% License: <hyperlink to license>
+%  
+% Author: _Mahtab Vafaee_, <mahtab.vafaee@gmail.com>
+%
+%  Change log:
+%  2023/11/15 MV Created  
+%  2024/02/06 MV Edited
+%  2024/04/15 MV/KM Revised
+% ----------------------------------------------------------------------
+%%
+
+clc; clear; close all;
+
+%% Plot settings
+
+cMap=jet(250);
+faceAlpha1=0.5;
+faceAlpha2=0.65;
+edgeColor1='none';
+edgeColor2='none';
+fontSize=25; 
+pColors=gjet(6);
+
+%% Using topology optimisation to create a density field
+
+% define box size, volume fraction as inputs for top.m
+nelx = 2*64; % 45
+nely = 2*24; % 20
+nelz = 2*24; % 20
+
+volfrac = 0.5;
+penal = 3;
+rmin = 10;
+
+v=1; % im2patch voxel 
+t=1; % shell thickness
+
+%% calculating the density distribution map
+[rho0] = top(nelx,nely,volfrac,penal,rmin);
+
+%% Extending in 3rd (z-direction) to create a 3D example
+ext = [1, 1, nelz];
+rho = repmat(rho0, ext); 
+
+sv3(rho); colormap warmcold; % Visualize 3D field
+
+%% Access model elements and patch data
+
+% Patch rho information to Elements and Faces
+[E,V]=im2patch(rho,true(size(rho)),'h',v);
+[F,rho_F] = element2patch(E,rho(:));
+
+% Boundary faces
+indBoundaryFaces = tesBoundary(F);
+Fb = F(indBoundaryFaces,:);
+
+% define face boundary rho values
+if size(Fb,2)==4
+    Fb = [Fb(:,[1 2 3]); Fb(:,[3 4 1]);]; % force to be triangles to be compatible with tet volume based in/out testing
+    rho_Fb = [rho_F(indBoundaryFaces); rho_F(indBoundaryFaces)];
+else
+    rho_Fb = rho_F(indBoundaryFaces);
+end
+
+%% Visualize 3D model and density data
+
+cFigure; 
+gpatch(Fb,V,rho_Fb,'k');
+axisGeom;
+drawnow; 
+
+%% Creating a grid  grid with a desired spacing (e.g. as per desired spatial frequency)
+
+spatialWaveLength = 6.66; % Nominal spatial wave length
+samplingRate = 4; % Rate per "wave" % 3
+voxelSize = spatialWaveLength/samplingRate;
+
+%% Interpolating model data onto grid with a desired spacing (e.g. as per desired spatial frequency)
+
+[rho_VG,X,Y,Z,DG_im, imOrigin]= toGrid (E,Fb,V,rho,voxelSize);
+
+%% Visualize interpolated density on the grid
+sv3(rho_VG); colormap warmcold;
+caxis([0,1]);
+
+%% Mapping the density field to the equivalent gyroid levelSet field
+
+l=(rho_VG-0.5)/-(1/3); 
+k = (2*pi)/spatialWaveLength; % number of periods
+
+%% Visualizig levelSet data
+cFigure;  
+scatter3(X(:),Y(:),Z(:),25,l(:),'filled');
+axis tight; axis equal; 
+colorbar;
+
+%% Evaluate triply periodic function
+
+S=(sin(k.*X).*cos(k.*Y))+(sin(k.*Y).*cos(k.*Z))+(cos(k.*X).*sin(k.*Z)); 
+sv3(S); colormap warmcold;
+
+%% Mapping the density field to the equivalent gyroid levelSet field
+% Gyroid "spans" -1.5-1.5, shift so minimum is at 1
+i=2.5; % Leads to a minimum of 1 later
+Sg=S+i; % S=-S-i;
+l=l+i; % l=-l-i; % i:i+s
+Sg=Sg./l; 
+
+Sg=Sg-1; % bringing back to zero levelset
+Sg=-Sg; % Negative 
+
+%% Construct iso-surface
+
+[Fg,Vg] = isosurface(X,Y,Z,Sg,0);
+
+Vg=Vg+imOrigin/2; % shifting vertices 
+
+%% Visualize surface gyroid function field
+
+sv3(Sg, voxelSize); colormap warmcold;
+hold on;
+gpatch(Fb,V,'bw', 'none', 0.5);
+axisGeom; 
+% colormap spectral; icolorbar; 
+camlight headlight; axis on;
+drawnow; 
+
+%% Visualization the distance image
+sv3(DG_im, voxelSize); hold on;
+
+% Visualize boundary surface
+gpatch(Fb,V,'kw','none',0.2);
+scatterV(imOrigin, 20,'filled')
+axisGeom; camlight headlight; axis on;
+drawnow; 
+
+%% create a shell boundary
+
+R=DG_im; % a copy of the distance matrix
+R(R<(-t/2))=-R(R<(-t/2))-t; % negate the distance field values at R=-t/2, and shift up
+
+Ls=(DG_im)<=-t; % Filtering the region where mapped gyroid is needed
+
+Sn=R; % R matrix for the whole image
+Sn(Ls)=Sg(Ls); % keep mapped gyroid for R<=-t
+
+%% Visualize mapped field with thickness
+
+sv3(Sn); colormap warmcold
+m=max(abs(Sn(:)));
+
+%% Construct iso-surface
+
+[Fsn,Vsn] = isosurface(X,Y,Z,Sn,0); % create a gyroid isosurface 
+
+%% Visualize surface
+cFigure; 
+gpatch(Fsn,Vsn,'kw','none',0.5);
+axisGeom; colormap spectral; icolorbar; 
+camlight headlight; axis off;
+drawnow; 
+
+%% Remove "Crumbs" and outer surface based on volume
+
+% Using grouping to keep only largest group
+groupOptStruct.outputType='label';
+[G,~,groupSize]=tesgroup(Fsn,groupOptStruct); %Group connected faces
+
+% Compute group volumes
+numGroups=max(G);
+volumeGroup = zeros(numGroups,1);
+for q=1:1:numGroups
+    logicCurrentGroup = G==q;
+    Fsn_group = Fsn(logicCurrentGroup,:);
+    volumeGroup(q) = patchVolume(Fsn_group,Vsn,1); 
+end
+
+volumeThreshold = 100; 
+
+% Keeping only the gyroid porous surface
+indGroupsKeep = find(volumeGroup>=volumeThreshold & volumeGroup==max(volumeGroup)); 
+logicCrop = ismember(G,indGroupsKeep);
+Fsn=Fsn(logicCrop,:); %Trim faces
+G=G(logicCrop,:); %Trim faces group labels
+volumeGroup=volumeGroup(indGroupsKeep); % Remaining volumes after crop
+
+[Fsn,Vsn]=patchCleanUnused(Fsn,Vsn); %Remove unused nodes
+
+%% Visualize surface
+
+cFigure; hold on;
+gpatch(Fsn,Vsn,'kw','none',0.5);
+axisGeom; colormap spectral; icolorbar; 
+camlight headlight; axis on;
+drawnow; 
+
+%%  Merging the faces and vertices
+
+[Fsn,Vsn]=mergeVertices(Fsn,Vsn); %Merge nodes
+[Fsn,Vsn]=patchCleanUnused(Fsn,Vsn); %Remove unused nodes
+
+%% Visualize surface
+
+cFigure; hold on;
+gpatch(Fsn,Vsn,'kw','none',1);
+gpatch(Fb,V,'rw','none',0.3);
+
+axisGeom; colormap spectral; icolorbar; 
+camlight headlight; axis on;
+drawnow; 
+
+%%  Join the two surfaces for STL version
+
+[Fsn,Vsn,Csn]=joinElementSets({Fb,Fsn},{V,Vsn});
+[Fsn,Vsn]=mergeVertices(Fsn,Vsn); %Merge nodes
+[Fsn,Vsn]=patchCleanUnused(Fsn,Vsn); %Remove unused nodes
